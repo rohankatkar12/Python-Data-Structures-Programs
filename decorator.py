d7c2a017fb77e65330c0ef2bbb477830246e0844
Rohan Katkar <rohankatkar1212@gmail.com>
2023-08-11 18:12:54 +0530

    Multiple decorators fuction added

--- decorator.py
+++ decorator.py
@@ -14,3 +14,39 @@ def div(a,b):
 
 obj = deco(div)
 print(obj(10,20))
+
+
+
+
+
+
+#Multiple Decorators
+
+def deco1(fun):
+    def wrapper_func():
+        print("Hii")
+        return fun()
+    return wrapper_func
+    
+def deco2(fun):
+    def wrapper_func():
+        print("whatsapp")
+        return fun()
+    return wrapper_func
+
+def deco3(fun):
+    def wrapper_func():
+        print("How Are You")
+        return fun()
+    return wrapper_func
+
+@deco1
+@deco2
+@deco3
+def show():
+    print("Rohan")
+    
+show()
+
+
+
